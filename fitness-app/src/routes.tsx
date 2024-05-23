b3f03c17b716075b9fa4f2ce8ceb37198c417caa
Adithi M Shrouthy <adithimshrouthy@gmail.com>
2024-05-23 10:56:13 +0200

    added home page link in app.tsx

--- fitness-app/src/routes.tsx
+++ fitness-app/src/routes.tsx
@@ -3,15 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import MuscleGroupMap from './components/MuscleGroupMap';
 import Exercises from './components/Excercises';
-<<<<<<< HEAD
 import SetGoal from './components/setGoal';
 import LogWorkout from './components/LogWorkout';
 import ProgressChart from './components/ProgressChart';
-import AuthForm from './services/AuthForm';
-=======
+
 import HomePage from './components/HomePage';
 import AuthForm from './components/AuthForm';
->>>>>>> 427ba92 (Added home page)
+
 
 const AppRoutes: React.FC = () => (
     <Router>
@@ -20,7 +18,6 @@ const AppRoutes: React.FC = () => (
             <Route path="/home" element={<HomePage />} />
             <Route path="/login" element={<AuthForm />} />
                 <Route index element={<MuscleGroupMap />} />
-                <Route path='/login' element={<AuthForm/>} />
                 <Route path="exercises/:muscleGroup" element={<Exercises />} />
                 <Route path="set-goal" element={<SetGoal />} />
                 <Route path="log-workout/:id" element={<LogWorkout />} />
