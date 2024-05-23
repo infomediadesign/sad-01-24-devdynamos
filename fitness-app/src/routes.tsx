86e90beb989d7a158a7d746add797303602498d8
Adithi M Shrouthy <adithimshrouthy@gmail.com>
2024-05-24 00:03:35 +0200

    updated routes

--- fitness-app/src/routes.tsx
+++ fitness-app/src/routes.tsx
@@ -6,23 +6,22 @@ import Exercises from './components/Excercises';
 import SetGoal from './components/setGoal';
 import LogWorkout from './components/LogWorkout';
 import ProgressChart from './components/ProgressChart';
-
 import HomePage from './components/HomePage';
 import AuthForm from './components/AuthForm';
-
+import Dashboard from './components/Dashboard';
 
 const AppRoutes: React.FC = () => (
     <Router>
         <Routes>
-            <Route path="/" element={<App />}>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/login" element={<AuthForm />} />
-                <Route index element={<MuscleGroupMap />} />
-                <Route path="exercises/:muscleGroup" element={<Exercises />} />
-                <Route path="set-goal" element={<SetGoal />} />
-                <Route path="log-workout/:id" element={<LogWorkout />} />
-                <Route path="progress/:id" element={<ProgressChart />} />
-            </Route>
+            <Route path="/" element={<App />} />
+            <Route path="home" element={<HomePage />} />
+            <Route path="login" element={<AuthForm />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="muscle-groups" element={<MuscleGroupMap />} /> {/* Assuming this is your index route */}
+            <Route path="exercises/:muscleGroup" element={<Exercises />} />
+            <Route path="set-goal" element={<SetGoal />} />
+            <Route path="log-workout/:id" element={<LogWorkout />} />
+            <Route path="progress/:id" element={<ProgressChart />} />
         </Routes>
     </Router>
 );
