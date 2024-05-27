3b5cb3b61ecda4c8be8804b69650dd7e3b0ac5de
Adithi M Shrouthy <adithimshrouthy@gmail.com>
2024-05-28 00:30:24 +0200

    updated the logic of login using jwt_tokens

---
+++ fitness-app/src/hooks/useAuth.ts
@@ -0,0 +1,38 @@
+// import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+// const useAuth = () => {
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const jwtToken = localStorage.getItem('jwt_token'); // Update key to "jwt_token"
+//     if (!jwtToken) {
+//       navigate('/');
+//     }
+//   }, [navigate]);
+// };
+
+// export default useAuth;
+
+
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const jwtToken = localStorage.getItem('jwt_token'); // Update key to "jwt_token"
+    if (jwtToken) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+      navigate('/');
+    }
+  }, [navigate]);
+
+  return isAuthenticated;
+};
+
+export default useAuth;
