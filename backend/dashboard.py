01df30ebdf925aaedc76c31358055b49af9b1dab
Shaikh Rayyan <shaikhmohdrayyan7@gmail.com>
2024-06-02 18:02:01 +0200

    added feature to dashboard

--- backend/dashboard.py
+++ backend/dashboard.py
@@ -4,12 +4,14 @@ import jwt
 from flask_cors import CORS
 from flasgger import swag_from
 from config import Config
+from datetime import datetime  # Correct import
 
 dashboard_bp = Blueprint('dashboard', __name__)
 CORS(dashboard_bp)
 
 def init_dashboard_routes(app, mongo):
     sessions_collection = mongo.db.sessions
+    calories_tracker_collection = mongo.db.calories_tracker  # Add reference to the calories tracker collection
 
     @app.before_request
     def authenticate():
@@ -51,5 +53,53 @@ def init_dashboard_routes(app, mongo):
             return jsonify({"error": "Unauthorized access"}), 401
 
         return jsonify({'message': f'Welcome to the dashboard, {g.user["username"]}'})
+
+    @dashboard_bp.route('/dashboard/current_week_progress', methods=['GET'])
+    @swag_from({
+        "tags": ["Dashboard"],
+        "summary": "Get Current Week Progress",
+        "description": "Retrieve the progress of the current week for the authenticated user.",
+        "responses": {
+            "200": {
+                "description": "Current week's progress retrieved successfully."
+            },
+            "400": {
+                "description": "Bad request or invalid data provided."
+            },
+            "401": {
+                "description": "Unauthorized access if token is missing or invalid."
+            }
+        },
+        "security": [
+            {
+                "Bearer": []
+            }
+        ]
+    })
+    def get_current_week_progress():
+        if not hasattr(g, 'user'):
+            return jsonify({"error": "Unauthorized access"}), 401
+
+        username = g.user['username']
+        current_date = datetime.now().date()
+
+        goals = list(calories_tracker_collection.find({'username': username}))
+        if not goals:
+            return jsonify({"error": "No active goals found"}), 400
+
+        current_week_goal = None
+        for goal in goals:
+            start_date = datetime.strptime(goal['start_date'], '%Y-%m-%d').date()
+            end_date = datetime.strptime(goal['end_date'], '%Y-%m-%d').date()
+            if start_date <= current_date <= end_date:
+                current_week_goal = goal
+                break
+
+        if not current_week_goal:
+            return jsonify({"error": "No goals set for this week ☹"}), 400
+
+        calories_burned = current_week_goal['calories_burned']
+
+        return jsonify({'calories_burned': calories_burned}), 200
 
     app.register_blueprint(dashboard_bp, url_prefix='/')
