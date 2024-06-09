87c7879e2c733b99161c8417b65148dc86c92671
Adithi M Shrouthy <adithimshrouthy@gmail.com>
2024-06-09 23:15:20 +0200

    removed extra lines

--- fitness-app/src/services/dashboardServices.ts
+++ fitness-app/src/services/dashboardServices.ts
@@ -12,8 +12,7 @@ export const getCalories = async (token: string) => {
     });
     return response.data;
   } catch (error: unknown) {
-    const typedError = error as ApiError; // Use type assertion
-    //Check each level of the object to avoid accessing properties of undefined
+    const typedError = error as ApiError; 
     if (typedError.response && typedError.response.data && typedError.response.data.error) {
       throw typedError.response.data.error;
     } else {
