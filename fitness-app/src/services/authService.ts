571b2d83cd9b5846a7f55ebde472dca82fae09fd
Adithi M Shrouthy <adithimshrouthy@gmail.com>
2024-06-09 20:40:44 +0200

    removed commented out lines

--- fitness-app/src/services/authService.ts
+++ fitness-app/src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000'; // Adjust the URL to your backend API
+const API_URL = 'http://127.0.0.1:5000'; 
 
 interface FormData {
   username: string;
