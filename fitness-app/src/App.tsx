73d434278a9b56eb0c7d91cf703af89882ca4a7b
Yamini-hari <yaminihari.germany@gmail.com>
2024-06-10 04:34:49 +0200

    minor refactoring

--- fitness-app/src/App.tsx
+++ fitness-app/src/App.tsx
@@ -1,26 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import AppRoutes from './routes';
+import React from 'react';
+import AppRoutes from './routes'; 
+
 import Footer from './components/common/Footer';
 import Navbar from './components/common/NavBar';
 
 const App: React.FC = () => {
-  const [token, setToken] = useState<string | null>(localStorage.getItem('token'));
-  const location = useLocation();
-
-  useEffect(() => {
-    setToken(localStorage.getItem('token'));
-  }, []);
-
-  // Determine if the Navbar should be shown based on the current path
-  const showNavbar = token && location.pathname.startsWith('/dashboard/*');
-
   return (
-    <div className="flex flex-col min-h-screen">
-      {showNavbar && <Navbar />}
-      <div className="flex-grow">
+    <div className="App">
+      <header className="App-header">
+        {/* <h1>FitSync</h1> */}
+      </header>
+      <main>
+      
         <AppRoutes />
-      </div>
+      </main>
       <Footer />
     </div>
   );
